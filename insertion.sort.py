72869ea060c9a23ed868e6da4c64bedcc3c71358
Kartik  K Bandekar <bandekar.kartik117@gmail.com>
2026-01-29 10:58:58 +0530

    Add files via upload

--- insertion.sort.py
+++ insertion.sort.py
@@ -1,29 +1,32 @@
-# def insertion_sort(arr):
-#     for i in range(1, len(arr)):
-#         key = arr[i]
-#         j = i - 1
+import random
+import time
+import matplotlib.pyplot as plt
 
-#         while j >= 0 and arr[j] > key:
-#             arr[j + 1] = arr[j]
-#             j -= 1
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
-#         arr[j + 1] = key
+n_list = [5000, 6000, 7000, 8000, 9000, 10000]
+sort_time = []
 
-#     return arr
+for n in n_list:
+    l = [random.randint(1, 100) for _ in range(n)]
+    s_t = time.time()
+    insertion_sort(l)
+    e_t = time.time()
+    sort_time.append(e_t - s_t)
+plt.plot(n_list, sort_time, marker='x') 
+plt.xlabel("Number of elements(n)")
+plt.ylabel("Time taken in seconds)")
+plt.title("Insertion Sort sort: Time Complexity Analysis")
+plt.grid(True)
+plt.savefig("insertion_sort.time_complexity.png", dpi=300,bbox_inches='tight')
+plt.show()
 
-
-# nums = [5, 2, 9, 1, 5, 6]
-# print(insertion_sort(nums))
-
-l = [1,2,3,4,8]
-print(l)
-n = len(l)
-for i in range (1,n):
-    j=i-1
-    v=l[i]
-    while j>=0 and l[j]>v:
-        l[j+1]=l[j]
-        j=j-1
-    l[j+1]=v
-print(l)
-        
+print("Input sizes:", n_list)
+print("Sorting times:", sort_time)
